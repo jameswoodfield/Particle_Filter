8156ebfb4f84cdeb3cb921d8aae2fc1d2774bfce
alobbe <33218137+alobbe@users.noreply.github.com>
2024-11-27 15:04:41 +0000

    add base class for models

---
+++ models/base.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class BaseModel(ABC):
+
+    def __init__(self, params):
+        self.params = params
+
+    @abstractmethod
+    def run(self, initial_state, n_steps):
+        pass
