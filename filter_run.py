1d5513fa71c9bf711d29ac7e243d8af41e8cc1ff
alobbe <33218137+alobbe@users.noreply.github.com>
2024-12-11 15:09:25 +0000

    comparison run of resampling vs no resampling

---
+++ filter_run.py
@@ -0,0 +1,32 @@
+import jax
+import jax.numpy as jnp
+from ml_collections import ConfigDict
+from models.ETD_KT_CM_JAX_Vectorised import ETD_KT_CM_JAX_Vectorised, ic, KS_params, KDV_params
+from filters.filter import ParticleFilter
+
+def run_filter(resampling):
+    signal_params = ConfigDict(KDV_params)
+    signal_params.update(E=1)
+    ensemble_params = ConfigDict(KDV_params)
+    ensemble_params.update(E=10)
+    ensemble_params.update(sigma=0.001)
+    signal_model = ETD_KT_CM_JAX_Vectorised(signal_params)
+    ensemble_model = ETD_KT_CM_JAX_Vectorised(ensemble_params)
+
+    initial_signal = ic(signal_model.x, signal_params.E)
+    initial_ensemble = ic(ensemble_model.x, ensemble_params.E) 
+    #initial_ensemble += 0.01 * jax.random.normal(jax.random.PRNGKey(56), initial_ensemble.shape) # adding this is totally "unphysical"
+    pf = ParticleFilter(
+        n_particles = ensemble_params.E,
+        n_steps = 100,
+        n_dim = initial_signal.shape[-1],
+        forward_model = ensemble_model,
+        signal_model = signal_model,
+        sigma = 0.1,
+        seed = 11,
+        resampling=resampling,
+    )
+    _, all = pf.run(initial_ensemble, initial_signal, 10)
+    signal = jnp.concatenate([initial_signal[None,...], all[1]], axis=0)
+    particles = jnp.concatenate([initial_ensemble[None,...], all[0]], axis=0)
+    return signal, particles, signal_model.x
